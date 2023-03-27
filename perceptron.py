15626833a9828626fe35224b6c671d74824ed2a0
sun301 <syun.tanaka0307@gmail.com>
2023-03-27 23:40:30 +0900

    modify AND function

--- perceptron.py
+++ perceptron.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def AND(x1, x2):
     w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1 * w1 + x2 * w2 + x2
@@ -5,8 +8,18 @@ def AND(x1, x2):
         return 0
     elif tmp > theta:
         return 1
-    
-    
+
+def AND(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
 if __name__ == '__main__':
     print(AND(1, 1))
     

32204957692c8d0a80928092d4cdb2419d8f1882
sun301 <syun.tanaka0307@gmail.com>
2023-03-27 23:49:31 +0900

    add NAND and OR function

--- perceptron.py
+++ perceptron.py
@@ -10,6 +10,26 @@ def AND(x1, x2):
         return 0
     else:
         return 1
+    
+def NAND(x1, x2):
+    x = nparray([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+    
+def OR(x1, x2):
+    x = nparray([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
 
 
 if __name__ == '__main__':
